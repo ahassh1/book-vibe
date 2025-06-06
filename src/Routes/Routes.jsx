57adaa49303103038e447b511e74ce4b4d7bfaea
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-06 19:14:29 +0600

    error page or page not found message shown when going to unknown route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
 import {
+  createBrowserRouter,
   RouterProvider,
 } from "react-router";
 import Root from '../pages/Root/Root';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
 
 export const router = createBrowserRouter([
   {
     path: "/",
     // element: <div>Hello World</div>,
-    Component:Root
+    Component:Root,
+    errorElement: <ErrorPage></ErrorPage>
   },
 ]);
--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div>
-            
+            <h2>Page Not Found tata</h2>
         </div>
     );
 };

6677057df2b8dd3a1aa4c8e82716cf37cd57f792
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-16 11:11:04 +0600

    rendaring home component

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 
 import {
   createBrowserRouter,
-  RouterProvider,
+
 } from "react-router";
 import Root from '../pages/Root/Root';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import Home from '../pages/Home/Home';
 
 export const router = createBrowserRouter([
   {
     path: "/",
     // element: <div>Hello World</div>,
     Component:Root,
-    errorElement: <ErrorPage></ErrorPage>
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index:true,
+        path:"/",
+        Component:Home
+      }
+    ]
   },
 ]);

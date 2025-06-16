9951e7d6475201a75f6846dc3fba631ff54aa3cb
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-16 18:18:12 +0600

    polishing the product card and adding react icons

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,26 +1,27 @@
 import React, { use } from 'react';
+import { FaStarHalfAlt } from "react-icons/fa";
 
 const Book = ({singleBook}) => {
     // const data= use(bookPromise)
     // console.log(data);
     console.log(singleBook);
-    const {image, author , bookName , category, publisher } = singleBook
+    const {image, author , bookName , category, publisher, rating  } = singleBook
     return (
-       <div className="card bg-base-100 w-96 shadow-sm">
-  <figure className='p-3'>
-    <img
+       <div className="card bg-base-100 w-96 shadow-sm border bg-teal-100 p-2">
+  <figure className='p-4 bg-gray-100 w-2/3 mx-auto'>
+    <img className='h-[170px]'
       src={image}
       alt="Shoes" />
   </figure>
   <div className="card-body">
     <h2 className="card-title">
-      Card Title
-      <div className="badge badge-secondary">NEW</div>
+     {bookName}
+      <div className="badge badge-secondary">{author}</div>
     </h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <p>{publisher}</p>
     <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
+      <div className="badge badge-outline">{category}</div>
+      <div className="badge badge-outline">{rating} <FaStarHalfAlt /></div>
     </div>
   </div>
 </div>

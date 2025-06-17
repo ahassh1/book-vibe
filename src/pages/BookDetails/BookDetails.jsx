4ba24388c9af693d4f56b09022007fdad87c46a3
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-17 15:19:44 +0600

    params and useParams use-case

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const BookDetails = () => {
+    const id = useParams();
+    console.log(id);
     return (
         <div>
             <h3>hello details</h3>
